7af05e3b5bfddd2915cc1ada223f58957fa063b0
Alexandru-Boiciuc <claudiub.alex@icloud.com>
2024-06-09 18:53:54 +0100

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-<script>
-      let array = ["red", "yellow", "green"];
-      let currentColor = 0;
-      function trafficLight() {
-          currentColor = (currentColor + 1) % array.length;
-          document.getElementById("colorButton").style.backgroundColor = array[currentColor];
-      }
-      setInterval(trafficLight, 10000);
-</script>
+let array = ["red", "yellow", "green"];
+let currentColor = 0;
+function trafficLight() {
+      currentColor = (currentColor + 1) % array.length;
+      document.getElementById("colorButton").style.backgroundColor = array[currentColor];
+}
+setInterval(trafficLight, 10000);
+
